3e63fce3af0e4573d43e3b3f5c7bae89bc2daae5
Hyeseong-int <a01025819869@gmail.com>
2021-06-16 15:29:50 +0900

    solved 1992 (재귀 구현 및 입력 수정)

--- Silver/1992.cpp
+++ Silver/1992.cpp
@@ -3,30 +3,46 @@
 using namespace std;
 
 //qt to string
-void qtst(int x, int y, int length, string &list, int** qt) {
-	if (length <= 2) {
-		if (qt[x][y] == qt[x][y + 1] && qt[x][y + 1] == qt[x + 1][y] && qt[x + 1][y] == qt[x + 1][y + 1]) {
-			cout << qt[x][y];
-		}
-		else {
-			cout << "(" << qt[x][y] << qt[x][y + 1] << qt[x + 1][y] << qt[x + 1][y + 1] << ")";
+void qtst(int x, int y, int length, int** qt) {
+	if (length == 1) {
+		cout << qt[x][y];
+		return;
+	}
+
+	bool oneOnly, zeroOnly;
+	oneOnly = zeroOnly = true;
+
+	for (int i = x; i < x + length; i++) {
+		for (int j = y; j < y + length; j++) {
+			if (qt[i][j] == 0)
+				oneOnly = false;
+			else if (qt[i][j] == 1)
+				zeroOnly = false;
 		}
 	}
-	else {
-		cout << "(";
-		qtst(x, y, (length / 2), list, qt);
-		qtst(x + (length / 2), y, length / 2, list, qt);
-		qtst(x, y + (length / 2), (length / 2), list, qt);
-		qtst(x + (length / 2), y + (length / 2), (length / 2), list, qt);
-		cout << ")";
+	if (oneOnly == true) {
+		cout << 1;
+		return;
 	}
+	else if (zeroOnly == true) {
+		cout << 0;
+		return;
+	}
+
+	cout << "(";
+	qtst(x, y, (length / 2), qt);
+	qtst(x, y + (length / 2), (length / 2), qt);
+	qtst(x + (length / 2), y, length / 2, qt);
+	qtst(x + (length / 2), y + (length / 2), (length / 2), qt);
+	cout << ")";
+
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL); 
-
+	//overflow
 	int n;
 	cin >> n;
 
@@ -37,19 +53,22 @@ int main() {
 	}
 
 	for (int i = 0; i < n; i++) {
-		int low;
+		string low;
 		cin >> low;
 		
-		for (int j = n - 1; j >= 0; j--) {
-			qt[i][j] = low % 10;
-			low /= 10;
+		for (int j = 0; j < n; j++) {
+			qt[i][j] = (int)low[j] - 48;
 		}
 	}
-	string list = "";
-
-	qtst(0, 0, n, list, qt);
-
-	cout << list;
+	/*
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << qt[i][j];
+		}
+		cout << '\n';
+	}
+	*/
+	qtst(0, 0, n, qt);
 
 	delete[]qt;
 }
