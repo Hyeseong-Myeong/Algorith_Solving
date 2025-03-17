5062bb25160924e0d195e137fdcae9b1b9162ceb
Hyeseong-Myeong <a01025819869@gmail.com>
2025-03-17 10:55:54 +0900

    solved: programmers 42747

---
+++ Programmers/42747.py
@@ -0,0 +1,17 @@
+def solution(citations):
+    citations.sort(reverse = True)
+    answer = 0
+
+    for i in range(0, len(citations)):
+        if citations[i] > answer:
+            answer += 1
+        else:
+            break;
+    
+    return answer
+
+
+
+
+citations = [1,10,11]
+solution(citations)
